d9fa28353abb29f534eff2b83903398390b01bb6
Rushi <rpatel0223@gmail.com>
2023-12-21 12:15:25 -0600

    Pushing newest code

--- server.js
+++ server.js
@@ -23,35 +23,51 @@ wss.on('open', function() {
     wss.send(JSON.stringify(subscribeMsg));
 })
 
+
+
 wss.on('message', async function(message) {
     console.log("Message is: " + message);
     const currentEvent = JSON.parse(message)[0];
+
+    // all of the above needs no adjustment
 
     if(currentEvent.T === "n") {
         let companyImpact = 0;
         console.log("If Statement Reached");
         const gptRequest = {
-            "model": "gpt-3.5-turbo",
+            "model": "gpt-4",
             "messages": [
                 { "role": "system", "content": "Only respond with a number from 1 - 100 detailing the impact of the headline." },
-                { "role": "user", "content": "Given the headline '" + currentEvent.headline + "', show me a number from 1 to 100 detailing the impact of this headline." }
+                { "role": "user", "content": "You are in charge of an investment portfolio. Your job is to analyze either the content of a current news headline or the contents of the article and decide one of 3 things. You must decide whether to purchase more of that stock, hold onto the current stock or sell our current shares depending on how the content of the article and how you predict it will affect the market. '" + currentEvent.headline + "', show me a number from 1 to 100 detailing the impact of this headline." }
             ]
         };
 
         async function gptCall() {
             console.log("OpenAI API Called");
+            
             try {
                 const completion = await openai.chat.completions.create(gptRequest);
                 console.log(completion.choices[0]);
 
-                const tickerSymbol = currentEvent.symbols[0];
-                companyImpact = parseInt(data.choice[0].message.content);
+                let tickerSymbol = currentEvent.symbols[0];
+                companyImpact = parseInt(completion.choices[0].message.content);
 
 
                 if (companyImpact >= 70) {
-
+                    console.log("Buy reached")
+                        let order = await alpaca.createOrder({
+                            symbol: tickerSymbol,
+                            qty: 1,
+                            side: 'buy',
+                            type: 'market',
+                            time_in_force: 'day'
+                        })
                 } else if(companyImpact <= 30) {
+                    console.log("Sell Reached")
+                    let closedPosition = alpaca.closedPosition(tickerSymbol);
 
+                    } else {
+                        console.log("Nothing reached")
                     }
                 
             } catch (error) {
